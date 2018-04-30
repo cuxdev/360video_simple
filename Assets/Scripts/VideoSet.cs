2c2428c2236f93e72575307c35a02d8d731759d2
Jesse Mullen <jesse.mullen@disney.com>
2018-04-30 09:03:47 -0700

    Swap video

--- Assets/Scripts/VideoSet.cs
+++ Assets/Scripts/VideoSet.cs
@@ -12,20 +12,27 @@ public class VideoSet : MonoBehaviour {
         player = transform.GetComponent<VideoPlayer>();
         if (player != null){
 
-            string path = Application.streamingAssetsPath + "/Flyover.mp4";
+            string path = Application.streamingAssetsPath + "/M3.mp4";
 
             Debug.Log("PATH - " + path);
             player.url = path;
             player.Prepare();
             player.Play();
 
+            //Application.targetFrameRate = 60;
+
         }
 	}
 
-	
-	
+
+	private void Start()
+	{
+        
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+        
+        //Debug.Log("Player FR - " + player.frameRate);
 	}
 }

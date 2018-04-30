e10ad657f6ae185a97a4ec714a40646fe9d341f7
Jesse Mullen <jesse.mullen@disney.com>
2018-04-30 08:42:22 -0700

    Changes for webGL

---
+++ Assets/Scripts/VideoSet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoSet : MonoBehaviour {
+
+    VideoPlayer player;
+
+	private void Awake()
+	{
+        player = transform.GetComponent<VideoPlayer>();
+        if (player != null){
+
+            string path = Application.streamingAssetsPath + "/Flyover.mp4";
+
+            Debug.Log("PATH - " + path);
+            player.url = path;
+            player.Prepare();
+            player.Play();
+
+        }
+	}
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
